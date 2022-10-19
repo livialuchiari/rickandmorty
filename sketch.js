6319369fdfc0cfc5db8f8f63ab22893dad340251
Lívia Luchiari <105221491+livialuchiari@users.noreply.github.com>
2022-10-19 15:12:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var background;
+
+
+function preload(){
+  
+  
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adicionando a imagem de fundo
+  background = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+  
+  
+
+//criando o sprite do jogador
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+  //movendo o jogador para cima e para baixo e tornando o jogo compatível com dispositivos móveis usando toques
+
+
+//solte balas e mude a imagem do atirador para a posição de tiro quando a tecla de espaço for pressionada
+
+
+drawSprites();
+
+
+}
